ba0cbb5aa2fa1ec09cd98680aed12334222db541
Yash Jain <yjain56@yahoo.com>
2022-07-16 16:53:04 +0530

    feat: added page for getting hoti details

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,10 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAgLrLE5qN9yLHeNXYiBrtZgsiyCWaa0EU",
   authDomain: "ljnm-app.firebaseapp.com",
@@ -17,3 +14,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const getFirebaseFirestoreDB = async () => getFirestore(app);

03be06022e31a283b9de53ff8110da547b7bf305
Yash Jain <yjain56@yahoo.com>
2022-07-16 08:18:35 +0530

    Initial commit, firebase integrated

---
+++ src/firebase/index.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAgLrLE5qN9yLHeNXYiBrtZgsiyCWaa0EU",
+  authDomain: "ljnm-app.firebaseapp.com",
+  projectId: "ljnm-app",
+  storageBucket: "ljnm-app.appspot.com",
+  messagingSenderId: "560867999210",
+  appId: "1:560867999210:web:457dd3ac62beb323fe4693",
+  measurementId: "G-C1ZTZ5XWCE",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
